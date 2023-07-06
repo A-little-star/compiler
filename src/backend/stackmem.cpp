8892774fb7872c04b2bcdc024ac2723b01352c07
A-little-star <2621939373@qq.com>
2023-07-06 17:07:33 +0800

    Update stack memory allocation model.

--- src/backend/stackmem.cpp
+++ src/backend/stackmem.cpp
@@ -1,10 +1,20 @@
 #include "../midend/xc.hpp"
 #include <unordered_map>
 #include <algorithm>
+#include <assert.h>
 
 std::unordered_map<value_ptr, int> off_map;
 std::unordered_map<value_ptr, int> arg_off_map;
 std::unordered_map<func_ptr, int> has_call;
+
+int SizeOfType(type_kind ty) {
+    switch (ty.tag) {
+        case KOOPA_TYPE_ARRAY: return SizeOfType(*ty.data.array.base) * ty.data.array.len;
+        case KOOPA_TYPE_POINTER: return 4;
+        case KOOPA_TYPE_INT32: return 4;
+        default: assert(false);
+    }
+}
 
 int CalStackMem(const func_ptr func) {
     int s = 0, r = 0, a = 0;
@@ -13,7 +23,12 @@ int CalStackMem(const func_ptr func) {
         for (size_t k = 0; k < bb->insts->len; k ++ ) {
             value_ptr inst = (value_ptr)bb->insts->buffer[k];
             switch (inst->kind.tag) {
-                case IR_ALLOC: s += 4; break;
+                case IR_ALLOC: {
+                    s += 4;
+                    type_kind type = *inst->ty.data.pointer.base;
+                    s += SizeOfType(type);
+                    break;
+                }
                 case IR_BINARY: s += 4; break;
                 case IR_LOAD: s += 4; break;
                 case IR_CALL: {
@@ -46,9 +61,9 @@ int CalStackMem(const func_ptr func) {
             switch (inst->kind.tag) {
                 case IR_CALL: {
                     if (inst->ty.tag == KOOPA_TYPE_INT32) {
-                        off_map[inst] = s + a - 4 * num - 4;
+                        off_map[inst] = s + a - num - 4;
                         inst->offset = off_map[inst];
-                        num ++;
+                        num += 4;
                     }
                     for (size_t i = 8; i < inst->kind.data.call.args->len; i ++ ) {
                         value_ptr arg = (value_ptr)inst->kind.data.call.args->buffer[i];
@@ -57,15 +72,25 @@ int CalStackMem(const func_ptr func) {
                     }
                     break;
                 }
-                case IR_ALLOC:
-                case IR_BINARY:
-                case IR_LOAD: {
-                    off_map[inst] = s + a - 4 * num - 4;
+                case IR_ALLOC: {
+                    off_map[inst] = s + a - num - 4;
                     inst->offset = off_map[inst];
-                    num ++;
+                    num += 4;
+                    num += SizeOfType(*inst->ty.data.pointer.base);
                     break;
                 }
-                default: break;
+                case IR_BINARY:
+                case IR_LOAD: {
+                    off_map[inst] = s + a - num - 4;
+                    inst->offset = off_map[inst];
+                    num += 4;
+                    break;
+                }
+                case IR_STORE:
+                case IR_BRANCH:
+                case IR_JUMP:
+                case IR_RETURN: break;
+                default: assert(false);
             }
         }
     }
